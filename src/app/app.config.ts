ecad649614370e0987c023400c20cea49b3b2799
anwar108658 <anwar108658@gmail.com>
2025-12-19 18:14:23 +0500

    add popup form

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,26 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes';
+
+const MyAura = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#eff6ff',
+      100: '#dbeafe',
+      200: '#bfdbfe',
+      300: '#93c5fd',
+      400: '#60a5fa',
+      500: '#3b82f6', // 👈 main color (blue)
+      600: '#2563eb',
+      700: '#1d4ed8',
+      800: '#1e40af',
+      900: '#1e3a8a'
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,7 +28,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: MyAura,
         options: {
           darkModeSelector: false,
           cssLayer: {
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Button } from "primeng/button";
+import { DialogModule } from 'primeng/dialog';
+import { DrawerModule } from 'primeng/drawer';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule,Button,DrawerModule,DialogModule,InputTextModule,FloatLabelModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  visible:boolean = false;
+  popupVisible:boolean = true;
 
+  sideMenu = signal([
+    {id:1,title:"Today",icon:"pi pi-pen-to-square"},
+    {id:2,title:"Pending",icon:"pi pi-calendar-clock"},
+    {id:3,title:"Completed",icon:"pi pi-check"},
+  ])
+
+  todoForm = new FormGroup({
+    title:new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(30)]),
+    priority:new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    deadLine:new FormControl('',[Validators.required]),
+    comment:new FormControl('',[Validators.required,Validators.minLength(1),Validators.maxLength(500)]),
+  })
 }
